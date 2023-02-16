056697ecccde33e83df38d7d17945c71800efb26
haaannn123 <haaannn97@gmail.com>
2023-02-16 14:15:40 -0500

    Add User login backend endpoint

---
+++ backend/routes/api/users.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const router = express.Router();
+
+
+// fetch('/api/session', {
+//     method: 'POST',
+//     headers: {
+//       "Content-Type": "application/json",
+//       "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
+//     },
+//     body: JSON.stringify({ credential: 'Demo-lition', password: 'password' })
+//   }).then(res => res.json()).then(data => console.log(data));
+
+
+//   fetch('/api/session', {
+//     method: 'POST',
+//     headers: {
+//       "Content-Type": "application/json",
+//       "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
+//     },
+//     body: JSON.stringify({ credential: 'demo@user.io', password: 'password' })
+//   }).then(res => res.json()).then(data => console.log(data));
+
+
+//   fetch('/api/session', {
+//     method: 'POST',
+//     headers: {
+//       "Content-Type": "application/json",
+//       "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
+//     },
+//     body: JSON.stringify({ credential: 'Demo-lition', password: 'Hello World!' })
+//   }).then(res => res.json()).then(data => console.log(data));
+module.exports = router;
