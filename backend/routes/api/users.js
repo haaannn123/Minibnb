a2689cbacb8bb2a08784c090f9c449d7abb49747
haaannn123 <haaannn97@gmail.com>
2023-02-16 14:28:43 -0500

    Add User input validation on user login backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -6,44 +6,6 @@ const { User } = require('../../db/models');
 const router = express.Router();
 
 
-// fetch('/api/session', {
-//     method: 'POST',
-//     headers: {
-//       "Content-Type": "application/json",
-//       "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
-//     },
-//     body: JSON.stringify({ credential: 'Demo-lition', password: 'password' })
-//   }).then(res => res.json()).then(data => console.log(data));
-
-
-//   fetch('/api/session', {
-//     method: 'POST',
-//     headers: {
-//       "Content-Type": "application/json",
-//       "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
-//     },
-//     body: JSON.stringify({ credential: 'demo@user.io', password: 'password' })
-//   }).then(res => res.json()).then(data => console.log(data));
-
-
-//   fetch('/api/session', {
-//     method: 'POST',
-//     headers: {
-//       "Content-Type": "application/json",
-//       "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
-//     },
-//     body: JSON.stringify({ credential: 'Demo-lition', password: 'Hello World!' })
-//   }).then(res => res.json()).then(data => console.log(data));
-
-
-// fetch('/api/session', {
-//     method: 'DELETE',
-//     headers: {
-//       "Content-Type": "application/json",
-//       "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
-//     }
-//   }).then(res => res.json()).then(data => console.log(data));
-
 router.post(
     '/',
     async (req, res) => {
@@ -58,16 +20,4 @@ router.post(
     }
   );
 
-  fetch('/api/users', {
-    method: 'POST',
-    headers: {
-      "Content-Type": "application/json",
-      "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
-    },
-    body: JSON.stringify({
-      email: 'spidey@spider.man',
-      username: 'Spidey',
-      password: 'password'
-    })
-  }).then(res => res.json()).then(data => console.log(data));
 module.exports = router;
