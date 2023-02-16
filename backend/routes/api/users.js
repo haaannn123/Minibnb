5a654ebc68b7ebd134b744252bc3f566a18f5321
haaannn123 <haaannn97@gmail.com>
2023-02-16 14:21:14 -0500

    Add User signup backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -1,4 +1,8 @@
-const express = require('express')
+const express = require('express');
+
+
+const { setTokenCookie, requireAuth } = require('../../utils/auth');
+const { User } = require('../../db/models');
 const router = express.Router();
 
 
@@ -32,11 +36,38 @@ const router = express.Router();
 //   }).then(res => res.json()).then(data => console.log(data));
 
 
-fetch('/api/session', {
-    method: 'DELETE',
+// fetch('/api/session', {
+//     method: 'DELETE',
+//     headers: {
+//       "Content-Type": "application/json",
+//       "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
+//     }
+//   }).then(res => res.json()).then(data => console.log(data));
+
+router.post(
+    '/',
+    async (req, res) => {
+      const { email, password, username } = req.body;
+      const user = await User.signup({ email, username, password });
+
+      await setTokenCookie(res, user);
+
+      return res.json({
+        user: user
+      });
+    }
+  );
+
+  fetch('/api/users', {
+    method: 'POST',
     headers: {
       "Content-Type": "application/json",
       "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
-    }
+    },
+    body: JSON.stringify({
+      email: 'spidey@spider.man',
+      username: 'Spidey',
+      password: 'password'
+    })
   }).then(res => res.json()).then(data => console.log(data));
 module.exports = router;
