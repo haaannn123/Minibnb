2ca671530a0e2084eda24d95bb3cd4745e3ddba1
haaannn123 <haaannn97@gmail.com>
2023-02-16 17:39:19 -0500

    finished phase 5

--- backend/db/models/user.js
+++ backend/db/models/user.js
@@ -5,8 +5,8 @@ const bcrypt = require('bcryptjs');
 module.exports = (sequelize, DataTypes) => {
   class User extends Model {
     toSafeObject() {
-      const { id, username, email } = this; // context will be the User instance
-      return { id, username, email };
+      const { id, username, email, firstName, lastName } = this; // context will be the User instance
+      return { id, username, email, firstName, lastname };
     }
     validatePassword(password) {
       return bcrypt.compareSync(password, this.hashedPassword.toString());
@@ -28,12 +28,14 @@ module.exports = (sequelize, DataTypes) => {
         return await User.scope('currentUser').findByPk(user.id);
       }
     }
-    static async signup({ username, email, password }) {
+    static async signup({ username, email, password, firstName, lastName }) {
       const hashedPassword = bcrypt.hashSync(password);
       const user = await User.create({
         username,
         email,
-        hashedPassword
+        hashedPassword,
+        firstName,
+        lastName
       });
       return await User.scope('currentUser').findByPk(user.id);
     }
@@ -70,6 +72,20 @@ module.exports = (sequelize, DataTypes) => {
         validate: {
           len: [60, 60]
         }
+      },
+      firstName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        validate: {
+          len: [1, 25],
+        }
+      },
+      lastName: {
+        allowNull: false,
+        type: DataTypes.STRING,
+        validate: {
+          len: [1, 25]
+        }
       }
     },
     {
@@ -82,7 +98,7 @@ module.exports = (sequelize, DataTypes) => {
       },
       scopes: {
         currentUser: {
-          attributes: { exclude: ["hashedPassword"] }
+          attributes: { exclude: ["hashedPassword", "createdAt", "updatedAt"] }
         },
         loginUser: {
           attributes: {}
--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -16,4 +16,6 @@ router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
 
+
+
 module.exports = router;
