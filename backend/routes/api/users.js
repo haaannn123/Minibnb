0ff9edb0eed61429d5c97a64e1e6441139babb3e
haaannn123 <haaannn97@gmail.com>
2023-02-16 14:18:03 -0500

    Add User logout backend endpoint

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -30,4 +30,13 @@ const router = express.Router();
 //     },
 //     body: JSON.stringify({ credential: 'Demo-lition', password: 'Hello World!' })
 //   }).then(res => res.json()).then(data => console.log(data));
+
+
+fetch('/api/session', {
+    method: 'DELETE',
+    headers: {
+      "Content-Type": "application/json",
+      "XSRF-TOKEN": `<value of XSRF-TOKEN cookie>`
+    }
+  }).then(res => res.json()).then(data => console.log(data));
 module.exports = router;
