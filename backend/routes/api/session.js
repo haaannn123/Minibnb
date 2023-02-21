343337843c6d571bceaa653711416874de1bdcb2
haaannn123 <haaannn97@gmail.com>
2023-02-21 18:35:49 -0500

    finally finished with setting up

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -6,49 +6,49 @@ const { check } = require('express-validator');
 const { handleValidationErrors } = require('../../utils/validation');
 const router = express.Router();
 
-// router.post(
-//     '/',
-//     async (req, res, next) => {
-//       const { credential, password } = req.body;
+router.post(
+    '/',
+    async (req, res, next) => {
+      const { credential, password } = req.body;
 
-//       const user = await User.login({ credential, password });
+      const user = await User.login({ credential, password });
 
-//       if (!user) {
-//         const err = new Error('Login failed');
-//         err.status = 401;
-//         err.title = 'Login failed';
-//         err.errors = { credential: 'The provided credentials were invalid.' };
-//         return next(err);
-//       }
+      if (!user) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = { credential: 'The provided credentials were invalid.' };
+        return next(err);
+      }
 
-//       await setTokenCookie(res, user);
+      await setTokenCookie(res, user);
 
-//       return res.json({
-//         user: user
-//       });
-//     }
-//   );
+      return res.json({
+        user: user
+      });
+    }
+  );
 
-// router.delete(
-//     '/',
-//     (_req, res) => {
-//       res.clearCookie('token');
-//       return res.json({ message: 'success' });
-//     }
-//   );
+router.delete(
+    '/',
+    (_req, res) => {
+      res.clearCookie('token');
+      return res.json({ message: 'success' });
+    }
+  );
 
-// router.get(
-//     '/',
-//     restoreUser,
-//     (req, res) => {
-//       const { user } = req;
-//       if (user) {
-//         return res.json({
-//           user: user.toSafeObject()
-//         });
-//       } else return res.json({ user: null });
-//     }
-//   );
+router.get(
+    '/',
+    restoreUser,
+    (req, res) => {
+      const { user } = req;
+      if (user) {
+        return res.json({
+          user: user.toSafeObject()
+        });
+      } else return res.json({ user: null });
+    }
+  );
 
 const validateLogin = [
     check('credential')
