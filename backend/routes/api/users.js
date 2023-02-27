3b91d8fa7a722f6a40c2277d73e76e48ac0cc826
haaannn123 <haaannn97@gmail.com>
2023-02-27 00:13:35 -0500

    changes to signup

--- backend/routes/api/users.js
+++ backend/routes/api/users.js
@@ -23,6 +23,14 @@ const validateSignup = [
       .exists({ checkFalsy: true })
       .isLength({ min: 6 })
       .withMessage('Password must be 6 characters or more.'),
+    check('firstName')
+      .exists({checkFalsy: true})
+      .isLength({min: 1})
+      .withMessage('Please provide a valid first name.'),
+    check('lastName')
+      .exists({checkFalsy: true})
+      .isLength({min: 1})
+      .withMessage('Please provide a valid last name'),
     handleValidationErrors
   ];
 
