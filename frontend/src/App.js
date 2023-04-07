d9677df12e0375bbd331c8ee88cee747d591c874
haaannn123 <haaannn97@gmail.com>
2023-04-07 00:53:36 -0400

    log in

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useDispatch } from "react-redux";
 import { Route, Switch } from "react-router-dom";
-import LoginFormPage from "./components/LoginFormPage";
-import SignupFormPage from "./components/SignUpFormPage";
-import Navigation from "./components/Navigation";
+import SignupFormPage from "./components/SignupFormPage";
 import * as sessionActions from "./store/session";
+import Navigation from "./components/Navigation";
 
 function App() {
   const dispatch = useDispatch();
@@ -15,19 +14,15 @@ function App() {
 
   return (
     <>
-    <Navigation isLoaded={isLoaded}/>
-    {isLoaded && (
-      <Switch>
-        <Route path="/login">
-          <LoginFormPage />
-        </Route>
-        <Route path="/signup">
-          <SignupFormPage />
-        </Route>
-      </Switch>
-    )}
+      <Navigation isLoaded={isLoaded} />
+      {isLoaded && (
+        <Switch>
+          <Route path="/signup">
+            <SignupFormPage />
+          </Route>
+        </Switch>
+      )}
     </>
-
   );
 }
 
