d4b7f6bcea5e6e5d0724bc45e4d5ee3929a2012c
haaannn123 <haaannn97@gmail.com>
2023-04-16 23:42:08 -0400

    render hotfix

--- frontend/src/components/GetAllSpots/index.js
+++ frontend/src/components/GetAllSpots/index.js
@@ -18,15 +18,15 @@ const GetAllSpots = () => {
         {spots.map((spot) => {
           return (
             <NavLink className="card" key={spot.id} to={`/spots/${spot.id}`} data-tooltip={spot.name}>
-                <img src={spot.previewImage} alt="house" className="card-image" />
+              <img src={spot.previewImage} alt="house" className="card-image" />
               <div className="card-location">
                 <h3>{`${spot.city}, ${spot.state}`}</h3>
                 <div className="stars-container">
                   <i className="fa-sharp fa-solid fa-star star"></i>
-                  <h3 className="rating">{spot.avgRating === 0 ? "New" : spot.avgRating.toFixed(1) }</h3>
+                  <h3 className="rating">{spot.avgRating === 0 ? "New" : spot?.avgRating?.toFixed(1)}</h3>
                 </div>
               </div>
-              <h4 className="spot-price">{`$${spot.price.toFixed(2)} night`}</h4>
+              <h4 className="spot-price">{`$${spot?.price?.toFixed(2)} night`}</h4>
             </NavLink>
           );
         })}
