7d078934f01789e30bb4a6650e2943f1084985b2
haaannn123 <haaannn97@gmail.com>
2023-07-01 19:23:55 -0400

    added past trips so users can see all the previous bookings

--- frontend/src/components/UsersTrips/index.js
+++ frontend/src/components/UsersTrips/index.js
@@ -18,44 +18,76 @@ const UsersTrips = () => {
     
     if (!userTrips) return null;
     
-    return(
-        < div className="user-trips-container">
-            <h1 className="user-trips-header">Upcoming Trips</h1>
-            <div className="user-trips-card-container">
-                {userTrips.length > 0? (userTrips.map(spotsObj => {
-                    return(
-                        <div className="destination-card" key={spotsObj.spotId}>
-                            <NavLink to={`/spots/${spotsObj.spotId}`}>
-                                {/* {console.log('ID HERE:', spotsObj.id)} */}
-                                {/* {console.log('SPOTSOBJ:', spotsObj)} */}
-                                <img className="destination-img" src={spotsObj.Spot.previewImage} alt="place"/>
-                            </NavLink>
-                            <div className="destination-details">
-                            <span className="destination-dates">
-                                {new Date(spotsObj.startDate).toLocaleDateString(undefined, {
-                                    month: "long",
-                                    day: "numeric",
-                                    timeZone: "UTC" // Replace with your desired time zone
-                                })} - {new Date(spotsObj.endDate).toLocaleDateString(undefined, {
-                                    month: "long",
-                                    day: "numeric",
-                                    timeZone: "UTC" // Replace with your desired time zone
-                                })}
-                            </span>
-                                <h2 className="destination-city">{spotsObj.Spot.city}</h2>
-                                <OpenModalButton 
-                                    buttonText="Change Reservation"
-                                    modalComponent={<ChangeReservationModal startDate={spotsObj.startDate} endDate={spotsObj.endDate} guests={spotsObj.numberOfGuests} maxGuests={spotsObj.Spot.guests} bookingId={spotsObj.id}/>}
-                                    className="change-booking"/>
+    const pastTrips = userTrips.filter(spotsObj => new Date(spotsObj.endDate) <= new Date());
+    const upcomingTrips = userTrips.filter(spotsObj => new Date(spotsObj.endDate) >= new Date());
+    return (
+        <>
+            <div className="user-trips-container">
+                <h1 className="user-trips-header">Upcoming Trips</h1>
+                <div className="user-trips-card-container">
+                    {upcomingTrips.length > 0 ? (
+                        upcomingTrips.map(spotsObj => (
+                            <div className="destination-card" key={spotsObj.spotId}>
+                                <NavLink to={`/spots/${spotsObj.spotId}`}>
+                                    <img className="destination-img" src={spotsObj.Spot.previewImage} alt="place" />
+                                </NavLink>
+                                <div className="destination-details">
+                                    <span className="destination-dates">
+                                        {new Date(spotsObj.startDate).toLocaleDateString(undefined, {
+                                            month: "long",
+                                            day: "numeric",
+                                            timeZone: "UTC"
+                                        })} - {new Date(spotsObj.endDate).toLocaleDateString(undefined, {
+                                            month: "long",
+                                            day: "numeric",
+                                            timeZone: "UTC"
+                                        })}
+                                    </span>
+                                    <h2 className="destination-city">{spotsObj.Spot.city}</h2>
+                                    <OpenModalButton
+                                        buttonText="Change Reservation"
+                                        modalComponent={<ChangeReservationModal startDate={spotsObj.startDate} endDate={spotsObj.endDate} guests={spotsObj.numberOfGuests} maxGuests={spotsObj.Spot.guests} bookingId={spotsObj.id} />}
+                                        className="change-booking"
+                                    />
+                                </div>
                             </div>
-                        </div>
-                )}
-                )): (
-                    <span>You don't have any trips coming up, make a reservation by clicking on a spot and selecting the appropriate dates and guests!</span>
-                )}
+                        ))
+                    ) : (
+                        <span>You don't have any trips coming up, make a reservation by clicking on a spot and selecting the appropriate dates and guests!</span>
+                    )}
+                </div>
             </div>
-        </div>
-    )
+            <div className="user-trips-container">
+                <h1 className="user-trips-header">Past trips</h1>
+                <div className="user-trips-card-container">
+                    {pastTrips.length > 0 ? (
+                        pastTrips.map(spotsObj => (
+                            <div className="destination-card" key={spotsObj.spotId}>
+                                <NavLink to={`/spots/${spotsObj.spotId}`}>
+                                    <img className="destination-img" src={spotsObj.Spot.previewImage} alt="place" />
+                                </NavLink>
+                                <div className="destination-details">
+                                    <span className="destination-dates">
+                                        {new Date(spotsObj.startDate).toLocaleDateString(undefined, {
+                                            month: "long",
+                                            day: "numeric",
+                                            timeZone: "UTC"
+                                        })} - {new Date(spotsObj.endDate).toLocaleDateString(undefined, {
+                                            month: "long",
+                                            day: "numeric",
+                                            timeZone: "UTC" 
+                                        })}
+                                    </span>
+                                    <h2 className="destination-city">{spotsObj.Spot.city}</h2>
+                                </div>
+                            </div>
+                        ))
+                    ) : null}
+                </div>
+            </div>
+        </>
+    );
+    
 }
 
 export default UsersTrips;
