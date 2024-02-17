a806d945e2b2a7f38a5401c23fba52cbe19f98bd
Han <haaannn97@gmail.com>
2024-02-17 11:52:50 -0500

    added a dynamic component file

---
+++ frontend/src/components/DisplayHouseInfo/index.js
@@ -0,0 +1,9 @@
+import './DisplayHouseInfo.css'
+
+const DisplayHouseInfo= (image, location, rating, guests, price, bedrooms, beds, baths) => {
+    return(
+        <div>Hello</div>
+    )
+}
+
+export default DisplayHouseInfo;
