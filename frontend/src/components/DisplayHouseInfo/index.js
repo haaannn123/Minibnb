1500b7bcc3fa3fdf43e8f9fa636326b98af534ff
Han <haaannn97@gmail.com>
2024-02-17 12:35:50 -0500

    added all necessary info about house

--- frontend/src/components/DisplayHouseInfo/index.js
+++ frontend/src/components/DisplayHouseInfo/index.js
@@ -1,10 +1,17 @@
 import './DisplayHouseInfo.css'
 
-const DisplayHouseInfo= ({image, location, rating, guests, price, bedrooms, beds, baths}) => {
+const DisplayHouseInfo= ({name, image, location, rating, guests, price, bedrooms, beds, baths}) => {
     return(
         <div>
-            <img src={image} alt="house"/>
-
+            {image && <div className="house-image">{image}</div>}
+            {name && <h3 className="house-name">{name}</h3>}
+            {location && <p className="house-location">{location}</p>}
+            {rating && <p className="house-rating">{rating}</p>}
+            {guests && <p className="house-guests">{guests}</p>}
+            {price && <p className="house-price">{price}</p>}
+            {bedrooms && <p className="house-bedrooms">{bedrooms}</p>}
+            {beds && <p className="house-beds">{beds}</p>}
+            {baths && <p className="house-baths">{baths}</p>}
         </div>
     )
 }
